14f66476b4fa0b42b4ddc3ed42bd6765a0b1c20c
Jeremy <jeremyborger@mac.com>
2015-11-24 21:37:33 -0600

    update
    merging changes

--- handlers.py
+++ handlers.py
@@ -1,4 +1,4 @@
-#This code is adapted from http://learn-2-code.appspot.com/ and https://www.udacity.com/course/viewer#!/c-nd000/l-4195258602/m-3971648689
+#This code is adapted from http://learn-2-code.appspot.com/ and https://www.udacity.com/course/viewer#!/c-nd000/l-4195258602/m-3971648689,  and https://cloud.google.com/appengine/docs/python/gettingstartedpython27/handlingforms
 #Bring in the following frameworks
 import os
 import cgi
@@ -10,33 +10,36 @@ from google.appengine.api import users
 from content import COURSES, TOPICS, SECTIONS
 import urllib
 
+#set guestbook var
 DEFAULT_GUESTBOOK_NAME = 'wall'
 
-#template = os.path.join(os.path.dirname(__file__), '/html_templates')
+#set template var
+template = os.path.join(os.path.dirname(__file__), '/html_templates')
+
+#setup Jinga
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader('html_templates'),
-    #loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+#function to define datastore key
 def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
-    """Constructs a Datastore key for a Guestbook entity.
-
-    We use guestbook_name as the key.
-    """
     return ndb.Key('Guestbook', DEFAULT_GUESTBOOK_NAME)
 
+#Class to define author object
 class Author(ndb.Model):
     """Sub model for representing an author."""
     identity = ndb.StringProperty(indexed=False)
     email = ndb.StringProperty(indexed=False)
 
+#Class to define posting object
 class Greeting(ndb.Model):
     """A main model for representing an individual Guestbook entry."""
     author = ndb.StructuredProperty(Author)
     content = ndb.StringProperty(indexed=False)
     date = ndb.DateTimeProperty(auto_now_add=True)
 
+#Below are handlers for the webapp to render pages
 class Handler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
 		self.response.out.write(*a, **kw)
@@ -68,30 +71,16 @@ class Guestbook(Handler):
     def get(self):
         guestbook_name = self.request.get('guestbook_name',DEFAULT_GUESTBOOK_NAME)
         greetings_query = Greeting.query(
-            ancestor=guestbook_key(DEFAULT_GUESTBOOK_NAME)).order(-Greeting.date)
+        ancestor=guestbook_key(DEFAULT_GUESTBOOK_NAME)).order(-Greeting.date)
         greetings = greetings_query.fetch(10)
 
-        user = users.get_current_user()
-        if user:
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
-      #  template_values = {
-         #  'user': user,
-         #  'greetings': greetings,
-         #  'guestbook_name': urllib.quote_plus(DEFAULT_GUESTBOOK_NAME),
-         #  'url': url,
-         #  'url_linktext': url_linktext,
-      #  }
+        template_values = {
+           'greetings': greetings,
+           'guestbook_name': urllib.quote_plus(DEFAULT_GUESTBOOK_NAME)
+        }
         
-        self.render('feedback.html', page_name="feedback")
+        self.render('feedback.html', page_name="feedback", greetings = greetings_query.fetch(10))
 
-    # Write Out Page here
-    #self.response.out.write(rendered_HTML)
-    #self.render('feedback.html', page_name='feedback/add')
     def post(self):
         # We set the same parent key on the 'Greeting' to ensure each
         # Greeting is in the same entity group. Queries across the
@@ -109,19 +98,12 @@ class Guestbook(Handler):
         # Get the content from our request parameters, in this case, the message
         # is in the parameter 'content'
         greeting.content = self.request.get('content')
-        greeting.put()
+        #check input server side for blank input
+        if not greeting.content.strip(" "):
+            self.redirect('/feedback/add')
+        else: 
+         # Write to the Google Database
+            greeting.put()
 
         query_params = {'guestbook_name': DEFAULT_GUESTBOOK_NAME}
         self.redirect('/?' + urllib.urlencode(query_params))
-
-    #  Get the content from our request parameters, in this case, the message
-    # is in the parameter 'content'
-    #check post for blank input and redirect the form back to itself.  If its valid create the post in the db and reload the main page.
-    
-    #if not greeting.content.strip(" "):
-        #self.redirect('/feedback/add')
-    #else: 
-    #  # Write to the Google Database
-    #post.put()
-      # Do other things here such as a page redirect
-    #self.redirect('/')
