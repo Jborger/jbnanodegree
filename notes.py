95018b69524b5ca2ff627d9be9728d5f37fb4f68
Jeremy <jeremyborger@mac.com>
2015-11-27 18:23:42 -0600

    format
    changed formatting

--- notes.py
+++ notes.py
@@ -1,14 +1,13 @@
 #This code is adapted from http://learn-2-code.appspot.com/ and https://www.udacity.com/course/viewer#!/c-nd000/l-4195258602/m-3971648689,  and https://cloud.google.com/appengine/docs/python/gettingstartedpython27/handlingforms
 
 import webapp2
-import cgi
 from handlers import *
 
 application = webapp2.WSGIApplication([
 	('/additional_resources/', ResourcesHandler),
 	('/nanodegree_notes/', NanodegreeHandler),
-  (r'/nanodegree_notes/course/(\d+)/', CourseHandler),
-  ('/feedback/', Guestbook),
-  ('/sign', Guestbook),
-  ('/*', MainPage)
+  	(r'/nanodegree_notes/course/(\d+)/', CourseHandler),
+  	('/feedback/', Guestbook),
+  	('/sign', Guestbook),
+  	('/*', MainPage)
 ], debug=True)
